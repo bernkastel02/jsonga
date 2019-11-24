09d9700dd38569860d426643694ebbae62618ec3
root <noscoper4ver@gmail.com>
2019-11-24 03:48:16 -0500

    deletiosn time kek

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const JsonGA = require(__dirname + "/../lib/JsonGA.js");
+const JsonGA = require("jsonga");
 const picture = new JsonGA.Picture(__dirname + "/demo.png");
 
 picture.format().then((pic) => {
