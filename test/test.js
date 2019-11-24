e6be9ca41a01c718e3d31b4e67133f52e173170c
root <noscoper4ver@gmail.com>
2019-11-24 04:09:59 -0500

    travis integration

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const JsonGA = require("jsonga");
+const JsonGA = require(__dirname + "/../lib/JsonGA");
 const picture = new JsonGA.Picture(__dirname + "/demo.png");
 
 picture.format().then((pic) => {
